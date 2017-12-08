53bbd34d896c0a2296f57812e3cefee285c0b4cf
David Norrish <norrish.d@gmail.com>
2017-12-08 18:09:40 +1100

    Add query text field, and a whole bunch of random elements
    Query field is not functional. Other changes:
    - Created reusable table element in a new app_component/ directory
    - Created an extremely small test csv file and a data/ directory

--- app.py
+++ app.py
@@ -5,23 +5,77 @@ Dash app homepage for Action Potential Party web app
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+from app_components.table import generate_table
 
 
 app = dash.Dash()
 app.css.append_css({"external_url":
                     "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+# Data for CSS
+background = '#111111'  # black
+text = '#7FDBFF'        # a shade of blue
+
+# Data for a table (of U.S. agriculture um....)
+# US_agri = pd.read_csv(
+#    'https://gist.githubusercontent.com/chriddyp/'
+#    'c78bf172206ce24f77d6363a2d754b59/raw/'
+#    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
+#    'usa-agricultural-exports-2011.csv')
+
+# my fake data
+basket = pd.read_csv('data/test.csv')
+
+# Demographic data: country, continent, population, life exp, GDP per capita
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/' +
+    'gdp-life-exp-2007.csv')
+
+# Data for personnel graph
 team_members = ['Liza', 'Nikunj', 'Noosh', 'Utkarsh']
 
-app.layout = html.Div(children=[
-    html.H1(children='Pyramidal Action Party'),
+# Text for a markdown section below
+health_text = """
+#
+# Global Demographic information
+### What we Care About
+We care about people getting to live an equally long time
 
-    html.Div(children='''
-        A multi-API information retrieval and NLP platform.
-    '''),
+### Why we Care
+According to the philosophical concept of the "veil of ignorance", WE could
+have been born somewhere poor!
+"""
+
+# Main page layout
+app.layout = html.Div(style={'backgroundColor': background}, children=[
+    html.H1(
+        children='Pyramidal Action Party',
+        style={
+            'textAlign': 'center',
+            'color': text
+        }),
+
+    html.Div(children='A multi-API information retrieval and NLP platform.',
+             style={
+                'textAlign': 'center',
+                'color': text,
+                'fontSize': '24px'
+             }),
+
+    html.Div(style={'textAlign': 'center'}, children=[
+
+        html.Label(children='Please enter your search query:', style={
+            'color': text}),
+
+        dcc.Input(value='cats', type='text')]
+    ),
 
     dcc.Graph(
-        id='example-graph2',
+        id='personnel-graph',
         figure={
             'data': [
                 {'x': team_members,
@@ -33,8 +87,44 @@ app.layout = html.Div(children=[
                      'Actually contributing to repo'}
             ],
             'layout': {
-                'title': 'Personnel Overview'
+                'title': 'Personnel Overview',
+                'plot_bgcolor': background,
+                'paper_bgcolor': background,
+                'font': {'color': text}
             }
+        }
+    ),
+
+    html.H4('Basket Analysis', style={'color': text}),
+
+    generate_table(basket),
+
+    dcc.Markdown(health_text, containerProps={'style': {'color': text}}),
+
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
         }
     )
 ])
